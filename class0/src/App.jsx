44e6d61d9de1be68835ec4b136d060baa1f7924a
Prem Kumar <premkumardavkn@gmail.com>
2026-08-31 23:00:27 +0530

    first commit

---
+++ class0/src/App.jsx
@@ -0,0 +1,18 @@
+
+// const app = () => {
+//   // return "Hello from app"
+
+//   return <div id="newDiv">
+//     <h1>Hello</h1>
+//     <h2>Hello</h2>
+//   </div>
+// }
+// export default app
+
+const App = () =>{
+  return <div>
+    <h1>Hello guys ,I am Prem</h1>
+    <h2>I am from Dehradun</h2>
+  </div>
+}
+export default App
