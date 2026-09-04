408f205ca27bb1c4a18c324b9238d614d3b85567
Prem Kumar <premkumardavkn@gmail.com>
2026-09-04 22:36:30 +0530

    new chnages

--- class0/src/App.jsx
+++ class0/src/App.jsx
@@ -9,10 +9,22 @@
 // }
 // export default app
 
-const App = () =>{
+
+// const App = () =>{
+//   return <div>
+//     <h1>Hello guys ,I am Prem</h1>
+//     <h2>I am from Dehradun</h2>
+//   </div>
+// }
+// export default App
+
+// Day 2
+import Nav from "./components/Nav.jsx"
+
+const App = () => {
   return <div>
-    <h1>Hello guys ,I am Prem</h1>
-    <h2>I am from Dehradun</h2>
+    <Nav />
   </div>
 }
+
 export default App
---
+++ class0/src/components/Nav.jsx
@@ -0,0 +1,7 @@
+const Nav = () =>{
+    return (
+        <div>This is nav</div>
+    )
+}
+
+export default Nav
