39743bcba7305c8f44f79559194bc1185a7654f8
Prem Kumar <premkumardavkn@gmail.com>
2026-09-16 22:18:01 +0530

    Digital Clock with dropdown for color change

--- DigitalClock/src/App.jsx
+++ DigitalClock/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import Clock from './Clock'
 const App = () => {
+  const [color, setColor] = useState('white')
   return (
     <div>
-      <clock />
+      <h1>Digital clock in React JS</h1>
+      <select onChange={(event) => setColor(event.target.value)}>
+        <option value={"white"}>Select Color</option>
+        <option value={"red"}>Red</option>
+        <option value={"blue"}>Blue</option>
+        <option value={"green"}>Green</option>
+        <option value={"orange"}>Orange</option>
+        <option value={"purple"}>Purple</option>
+      </select>
+      <Clock color={color} />
     </div>
   )
 }
 
 export default App
+ 
--- DigitalClock/src/Clock.jsx
+++ DigitalClock/src/Clock.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
-const Clock = () => {
-    cosnt[time, setTime] = useState(0);
+const Clock = ({color}) => {
+    const [time, setTime] = useState(0);
     useEffect(() => {
         setInterval(() => {
-            setTime(new DataTransfer().toLocaleTimeString());
+            setTime(new Date().toLocaleTimeString());
         }, 1000);
     }, [])
     return (
         <div>
-            <h1 style={color = 'green'}>{time}</h1>
+            <h1 style={{ color: color, backgroundColor: 'black', width: '120px', padding: '10px', borderRadius: '20px' }}>{time}</h1>
         </div >
     )
 }
